ff85a9d8306397e4f1b0f80fae6cd151ae797661
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-06-23 12:03:19 +0200

    Intermediate files in temp, final output in build

--- step1.sh
+++ step1.sh
@@ -1,4 +1,6 @@
 echo "Extract from source archive"
-tar --directory=kildedata -zxvf kildedata/Landskap.tar.gz 
+cd temp
+tar -zxvf ../kildedata/Landskap.tar.gz 
 echo "Convert to UTM GeoJSON"
-ogr2ogr -t_srs EPSG:32633 -f GeoJSON temp/landskap_32633.geojson kildedata/NIN_LA_HT_GT.shp 
+ogr2ogr -t_srs EPSG:32633 -f GeoJSON landskap_32633.geojson NIN_LA_HT_GT.shp 
+cd ..
